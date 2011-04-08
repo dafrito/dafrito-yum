482dfbef1f6f32c165006893bd9719dc7a70ba24
Aaron Faanes <dafrito@gmail.com>
2011-04-07 20:04:53 -0500

    Clean before creating RPMs
    I think this is a reasonable requirement, since we really want the most
    pristine builds possible. If it ends up being a performance hog, we can
    remove it or make it optional.

--- update.sh
+++ update.sh
@@ -2,6 +2,8 @@
 PATH=/bin:/usr/bin
 . $LIBDIR/functions.sh || exit 1
 
+CLEAN=clean
+
 REPODIR=$*
 REPODIR=${REPODIR:-.}
 [ -d "$REPODIR/repodata" ] || die "$REPODIR does not look like a yum repository"
@@ -16,7 +18,7 @@ sync_package() {
 	local name=$1
 	local path=$PROJECTDIR/$name
 	pushd $path || die "Failed to change directory to $path"
-	make rpm || die "Build failed for $name"
+	make $CLEAN rpm || die "Build failed for $name"
 	popd
 	for rpm in `rpm --specfile $RPMDIR/SPECS/$name.spec -q`; do
 		rpmfile="$RPMDIR/RPMS/`echo $rpm | sed -nre 's%^.*\.([^.]+)$%\1%p'`/$rpm.rpm"
