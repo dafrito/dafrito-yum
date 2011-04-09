9e49584d3b67c1c3f5f3200b93f4d0fbd04e6fc8
Aaron Faanes <dafrito@gmail.com>
2011-04-09 05:06:59 -0500

    add.sh doesn't need to do these unnecessary steps

--- add.sh
+++ add.sh
@@ -8,6 +8,4 @@ echo "Testing makefile using a dry-run.."
 make -n rpm >/dev/null || die "Package cannot be built using make"
 package_dir=`pwd`
 
-mkdir -p $CONFIGDIR/packages/
-cd $CONFIGDIR/packages
-ln -s $package_dir $CONFIGDIR/packages || die "Failed to create symlink to $package_dir"
+ln -v -s $package_dir $CONFIGDIR/packages || die "Failed to create symlink to $package_dir"
