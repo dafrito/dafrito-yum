6b91b1dd53b2abd1adce2291cb149ecdac6b521f
Aaron Faanes <dafrito@gmail.com>
2011-04-09 16:13:19 -0500

    Changed error message in push to be more accurate
    If rsync fails, then this command will fail. We'll either have to unroll
    this to get our accuracy, or just do what we're doing now, and be
    ambiguous. So far, it's been pretty obvious what went wrong.

--- push.sh
+++ push.sh
@@ -8,4 +8,4 @@ do_push() {
 REPODIR=`abs_path "${1:-.}"`
 is_yum_repo $REPODIR || die "$REPODIR does not look like a yum repository"
 createrepo -C -v $REPODIR/ # update the repository's metadata first
-do_push -n && confirm && do_push || die "Exiting on user input"
+do_push -n && confirm && do_push || die "Failed to push"
