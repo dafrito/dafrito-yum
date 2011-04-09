96d1a56527bb1c8069162f840936856ce9540861
Aaron Faanes <dafrito@gmail.com>
2011-04-09 17:32:19 -0500

    pull, push now handle REPODIR more carefully if it doesn't exist

--- pull.sh
+++ pull.sh
@@ -5,10 +5,12 @@ do_pull() {
 	rsync -ihavz $* "$REMOTE/*" $REPODIR/
 }
 
-REPODIR=`abs_path "${1:-.}"`
+REPODIR=${1:-.}
 if [ -d "$REPODIR" ]; then
+	REPODIR=`abs_path "$REPODIR"`
 	is_yum_repo $REPODIR || die "$REPODIR does not look like a yum repository"
-	do_pull -n && confirm && do_pull || die "Failed to pull"
 else
-	mkdir -v $REPODIR && do_pull || die "Repository could not be created"
+	mkdir -v $REPODIR || die
+	REPODIR=`abs_path "$REPODIR"`
 fi
+do_pull -n && confirm && do_pull || die "Failed to pull"
--- push.sh
+++ push.sh
@@ -5,7 +5,9 @@ do_push() {
 	rsync -ihavz --delete $* $REPODIR/* $REMOTE/
 }
 
-REPODIR=`abs_path "${1:-.}"`
+REPODIR=${1:-.}
+[ -d $REPODIR ] || die "$REPODIR is not a directory"
+REPODIR=`abs_path "$REPODIR"`
 is_yum_repo $REPODIR || die "$REPODIR does not look like a yum repository"
 createrepo -C -v $REPODIR/ # update the repository's metadata first
 do_push -n && confirm && do_push || die "Failed to push"
