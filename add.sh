344fc4e7a85d64ffb3e6308ef740df47eaa015f5
Aaron Faanes <dafrito@gmail.com>
2011-04-09 05:19:14 -0500

    Leave package directory once we're doing checking it

--- add.sh
+++ add.sh
@@ -3,9 +3,10 @@
 package="${*:-.}"
 
 [ -d $package ] || die "Not a package directory: $package"
-cd $package || die "Package is not accessible: $package"
+pushd $package || die "Package is not accessible: $package"
 echo "Testing makefile using a dry-run.."
 make -n rpm >/dev/null || die "Package cannot be built using make"
 package_dir=`pwd`
+popd
 
 ln -v -s $package_dir $CONFIGDIR/packages || die "Failed to create symlink to $package_dir"
