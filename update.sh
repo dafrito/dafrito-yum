d9314540c6c707522aa8469d51b86d4c3bd8bb4c
Aaron Faanes <dafrito@gmail.com>
2011-04-09 04:55:25 -0500

    update.sh now uses local variables when it can

--- update.sh
+++ update.sh
@@ -17,8 +17,8 @@ sync_package() {
 	make rpm || die "Build failed for $name"
 	popd
 	for rpm in `rpm --specfile $RPMDIR/SPECS/$name.spec -q`; do
-		arch=`echo $rpm | grep -E -o '[^.]+$'`
-		rpmfile="$RPMDIR/RPMS/$arch/$rpm.rpm"
+		local arch=`echo $rpm | grep -E -o '[^.]+$'`
+		local rpmfile="$RPMDIR/RPMS/$arch/$rpm.rpm"
 		if [ ! -f $REPODIR/`basename $rpmfile` ]; then
 			cp -v -p $rpmfile $REPODIR || die "Failed to copy package: $rpmfile"
 			unsigned="$unsigned $rpm.rpm"
